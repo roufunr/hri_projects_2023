0a3d8ee79b6cad8286d1e8ed9a5fe58b5ddf0d0b
roufunr <rouf@nevada.unr.edu>
2023-09-07 05:02:09 -0700

    added code for talker

--- week0/scripts/talker.py
+++ week0/scripts/talker.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
-# license removed for brevity
 import rospy
 from std_msgs.msg import String
+from time import time
 
 def talker():
     pub = rospy.Publisher('chatter', String, queue_size=10)
     rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(0.25) # 10hz
     while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
+        hello_str = "hello world " + str(time())
         rospy.loginfo(hello_str)
         pub.publish(hello_str)
         rate.sleep()
