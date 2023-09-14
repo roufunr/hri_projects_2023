7a129bcba7267173d60a2ada01c33f1247ab1a31
roufunr <rouf@nevada.unr.edu>
2023-09-14 01:50:24 -0700

    gave some realistic movement to robot

--- week1/geometry_message_application.py
+++ week1/geometry_message_application.py
@@ -8,11 +8,11 @@ from time import time
 def talker():
     pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
     rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(0.3) # frequency
+    rate = rospy.Rate(0.5) # frequency
     while not rospy.is_shutdown():
         twist_msg = Twist()
-        twist_msg.linear = Vector3(x = 3.0, y = -4.0, z = 3.0)
-        twist_msg.angular = Vector3(x = 0.1, y = 0.2, z = 0.3)
+        twist_msg.linear = Vector3(x = 5, y = 0, z = 0)
+        twist_msg.angular = Vector3(x = 0.0, y = 0.0, z = 0.1)
         rospy.loginfo(twist_msg)
         pub.publish(twist_msg)
         rate.sleep()
